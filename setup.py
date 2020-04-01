67e30951feca6d4000e080de6a774a4815dbb854
StoresundHetland <oyvind.hetland@inmeta.no>
2020-04-02 00:16:59 +0200

    Moved reqs from .in file to setup.py. This prevents local absolute path to be injected into req.txt. pip-compile still works as usual :)

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="covid19", version="0.1", packages=find_packages(),
+    name="covid19", 
+    version="0.1", 
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pyjanitor',
+        'streamlit',
+        'altair',
+        'vega_datasets',
+    ]
 )
